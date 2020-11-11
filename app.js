7003fd58c5486417d770c8e4fbfc6c578067a81a
samudrareggi <samudrareggi1@gmail.com>
2020-11-11 16:58:38 +0700

    Push to new fork

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require("express")
 const port = 3000
 const app = express()
+const cors = require("cors")
 const routes = require("./routes")
 const errorHandler = require("./middlewares/errorHandler")
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+app.use(cors())
 app.use(routes)
 app.use(errorHandler)
 
-// app.listen(port, () => {
-//   console.log("LOVE U " + port)
-// })
+app.listen(port, () => {
+  console.log("LOVE U " + port)
+})
 
 module.exports = app
