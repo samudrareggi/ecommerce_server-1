4c970580b100b4f66ae7c2a49ae1f4c69bc3f20c
samudrareggi <samudrareggi1@gmail.com>
2020-11-17 14:06:49 +0700

    Remove auth in get category

--- routes/index.js
+++ routes/index.js
@@ -14,19 +14,19 @@ router.post("/register", UserController.register)
 router.get("/products", ProductController.getProduct)
 router.post("/login", UserController.login)
 router.get("/banners", BannerController.getBanner)
+router.get("/categories", CategoryController.getCategory)
+router.get("/products/:id", ProductController.getProductById)
+router.get("/banners/:id", authorization, BannerController.getBannerById)
 
 router.use(authentication)
-router.get("/products/:id", ProductController.getProductById)
 router.post("/products", authorization, ProductController.addProduct)
 router.put("/products/:id", authorization, ProductController.putProductById)
 router.patch("/products/:id", authorization, ProductController.patchProductById)
 router.delete("/products/:id", authorization, ProductController.deleteProductById)
 
-router.get("/categories", CategoryController.getCategory)
 router.post("/categories", authorization, CategoryController.addCategory)
 
 router.post("/banners", authorization, BannerController.addBanner)
-router.get("/banners/:id", authorization, BannerController.getBannerById)
 router.put("/banners/:id", authorization, BannerController.putBannerById)
 router.delete("/banners/:id", authorization, BannerController.deleteBannerById)
 
